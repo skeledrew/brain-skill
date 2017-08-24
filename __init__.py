290a9629959471be305edbfdeb9c375c5273f9a2
Andrew Phillips <skeledrew@gmail.com>
2017-08-24 11:43:54 -0500

    Add license

---
+++ __init__.py
@@ -0,0 +1,19 @@
+# This file is part of brain-skill
+
+# brain-skill - This is a Mycroft skill that is intended to be dynamically extensible and modifiable.
+
+# @author Andrew Phillips
+# @copyright 2017 Andrew Phillips <skeledrew@gmail.com>
+
+# brain-skill is free software; you can redistribute it and/or
+# modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or any later version.
+
+# brain-skill is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+
+# You should have received a copy of the GNU Affero General Public
+# License along with brain-skill.  If not, see <http://www.gnu.org/licenses/>.
